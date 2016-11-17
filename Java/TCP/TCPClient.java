99a2c7ab258bcca7e104bbbdaf0506163a03cee0
Daniel <dfzunigah@unal.edu.co>
2016-11-17 08:08:55 -0500

    TCP Connection

---
+++ Java/TCP/TCPClient.java
@@ -0,0 +1,32 @@
+import java.io.*; 
+import java.net.*; 
+
+class TCPClient { 
+	public static void main(String[] args) throws Exception{
+            String sentence="1";
+            String modifiedSentence;
+            
+            BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+            
+            Socket clientSocket = new Socket("10.203.2.16",6789); 
+
+		/*
+		Server always runs first.
+		If it runs between two computers: One runs client, the other runs server. In client must be the server IP.
+		If it runs on one computer: It need to have both client and server. First server (Run File) IP form that computer.
+		*/
+            
+            DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+            BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            
+            outToServer.writeBytes(sentence + '\n');
+            
+            modifiedSentence = inFromServer.readLine();
+            
+            System.out.println("FROM SERVER: " + modifiedSentence);
+            
+            clientSocket.close();
+
+    }
+    
+}
