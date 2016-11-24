e177c4ce3a68266aa0742ed7551f6b361937a478
Daniel <dfzunigah@unal.edu.co>
2016-11-23 19:15:32 -0500

    toast desktop example

---
+++ Java/Toast Message/Test.java
@@ -0,0 +1,51 @@
+package test;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+
+public class Test extends JFrame {
+
+    private JPanel contentPane;
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    Test frame = new Test();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public Test() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 450, 300);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        contentPane.setLayout(new BorderLayout(0, 0));
+        setContentPane(contentPane);
+
+        JButton btnTestToast = new JButton("Test Toast");
+        btnTestToast.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                ToastMessage toastMessage = new ToastMessage("Sample text to toast ",3000);
+                toastMessage.setVisible(true);
+            }
+        });
+        contentPane.add(btnTestToast, BorderLayout.SOUTH);
+    }
+
+}
+
